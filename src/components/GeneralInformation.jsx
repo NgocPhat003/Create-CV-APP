bbaa40f17050c98f0aa9c0f8755cde7a31a19101
NgocPhat003 <ngocphat2405@gmail.com>
2026-09-03 17:48:47 +0700

    finish general information component

--- src/components/GeneralInformation.jsx
+++ src/components/GeneralInformation.jsx
@@ -51,4 +51,33 @@ const GeneralInformation = ({ changePage, generalData, setGeneralData }) => {
         }
     };
     
+    return (
+        <section className="general-information">
+            <h2 className="h2-information-page">General Information</h2>
+            
+            <p className="information-description">
+                Fill fields with your name and contact information
+            </p>
+
+            <div className="form-block">
+                <FormGeneralInformation
+                    generalData={draft}
+                    setGeneralData={setDraft}
+                    errors={errors}
+                    validateFields={validateFields}
+                />
+
+                <div className="btn-block">
+                    <button className="form-btn" onClick={handleSaveData}>
+                        Save
+                    </button>
+                    <button className="form-btn" onClick={() => handleChangePage(1)}>
+                        Next &#8594;
+                    </button>
+                </div>
+            </div>
+        </section>
+    );
 };
+
+export default GeneralInformation;
